3087205b9dbef53a756b74621ec549da1f61ac38
Magnus Krona <magnus.krona@gmail.com>
2013-11-14 14:01:06 +0100

    Initial commit

---
+++ Longitude/Hubs/MapHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Longitude.Hubs
+{
+    public class MapHub : Hub
+    {
+        public void BroadcastPosition(string latitude, string longitude)
+        {
+            Clients.All.Send(latitude, longitude);
+        }
+
+    }
+}
---
+++ Longitude/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Longitude.Startup))]
+namespace Longitude
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+        }
+    }
+}
